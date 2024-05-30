495129f8427e713d1ca6f939be8195178840000e
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2024-05-30 01:02:32 -0500

    Initial commit

--- server/app.py
+++ server/app.py
@@ -22,10 +22,10 @@ def index():
 # def not_found(e):
 #     return render_template("index.html")
 
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-# def catch_all(path):
-#     return render_template("index.html")
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
