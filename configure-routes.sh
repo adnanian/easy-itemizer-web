9d4fb13cfa46158ae9850cdce848d56b0469495b
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2024-06-15 15:30:21 -0500

    Deployment Test 3 - add index to endpoint_whitelist

--- configure-routes.sh
+++ configure-routes.sh
@@ -4,4 +4,14 @@
 # created by Adnan Wazwaz.
 # Purpose is to configure 
 
-node client/configureClient.cjs
+# CURRENTLY IN DEVELOPMENT
+
+#node client/configureClient.cjs
+cd ~/Development/code/projects/phase-5-project/easy-itemizer-web
+pwd
+pipenv requirements > requirements.txt
+npm run build --prefix client
+gid add .
+git commit -m [message]
+git push origin HEAD
+git push origin HEAD:main
