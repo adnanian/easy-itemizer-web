56de8b60e30676a97a9999c9eabbfd6e7c1c2897
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2024-06-16 20:45:46 -0500

    Add styling to errors, filter items by publicity

--- server/resources/item_resource.py
+++ server/resources/item_resource.py
@@ -1,32 +1,42 @@
-from flask import request, g
+from flask import request, session
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
 from resources.dry_resource import DRYResource
 from config import db, api
 from models.models import Item
 
-class ItemResource(DRYResource):
+
+class ItemResource(Resource):
     """Resource tied to the Item model. Handles fetch requests for all Item instances.
 
     Args:
         DRYResource (DRYResource): simplify RESTFul API building.
     """
 
-    def __init__(self):
-        super().__init__(Item)
+    def get(self):
+        """_summary_"""
+        items = [
+            item.to_dict()
+            for item in Item.query.filter(
+                db.or_(Item.is_public == True, Item.user_id == session.get("user_id"))
+            )
+        ]
+        return items, 200
 
     def post(self):
         """Creates a new instance of Item.
 
-          Returns:
-              dict: a JSONified dictionary of the created Item and its attributes, if creation successful, otherwise an error message.
+        Returns:
+            dict: a JSONified dictionary of the created Item and its attributes, if creation successful, otherwise an error message.
         """
         try:
             new_item = Item(
-                name = request.get_json().get("name"),
-                description = request.get_json().get("description"),
-                image_url = request.get_json().get("image_url"),
-                part_number = request.get_json().get("part_number"),
-                is_public = request.get_json().get("is_public"),
-                user_id = request.get_json().get("user_id")
+                name=request.get_json().get("name"),
+                description=request.get_json().get("description"),
+                image_url=request.get_json().get("image_url"),
+                part_number=request.get_json().get("part_number"),
+                is_public=request.get_json().get("is_public"),
+                user_id=request.get_json().get("user_id"),
             )
             db.session.add(new_item)
             db.session.commit()
@@ -35,15 +45,17 @@ class ItemResource(DRYResource):
             print(e)
             return {"message": str(e)}, 422
 
+
 class ItemById(DRYResource):
     """Resource tied to the Item model. Handles fetch requests for single Item instances.
 
-      Args:
-          DRYResource (DRYResource): simplify RESTFul API building.
+    Args:
+        DRYResource (DRYResource): simplify RESTFul API building.
     """
 
     def __init__(self):
         super().__init__(Item)
-        
+
+
 api.add_resource(ItemResource, "/items")
 api.add_resource(ItemById, "/items/<int:id>", endpoint="item_by_id")
